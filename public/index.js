7b1617d7dda07bd586d11d3109e9a78b3d9e6484
nicoUnsam82 <nlgonzalez@estudiantes.unsam.edu.ar>
2022-02-10 21:02:52 -0300

    Solucion de problema en lado cliente

--- public/index.js
+++ public/index.js
@@ -1,2 +1,13 @@
 "use strict";
-var socket = io();
+Object.defineProperty(exports, "__esModule", { value: true });
+//USO DE LIBRERIA PARA CLIENTE EN TYPESCRIPT DE IO SOCKET (ADEMAS INSTALAR npm install socket.io-client)
+var socket_io_client_1 = require("socket.io-client");
+// please note that the types are reversed
+var socket = socket_io_client_1.io();
+//FIN DE USO DE //USO DE LIBRERIA PARA CLIENTE EN TYPESCRIPT DE IO SOCKET (ADEMAS INSTALAR npm install socket.io-client)
+var agregarProducto = document.getElementById("#productoAgregar");
+if (agregarProducto) {
+    agregarProducto.addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
+}
--- public/index.ts
+++ public/index.ts
@@ -1,1 +1,24 @@
-const socket:any = io();
+//USO DE LIBRERIA PARA CLIENTE EN TYPESCRIPT DE IO SOCKET (ADEMAS INSTALAR npm install socket.io-client)
+import { io, Socket } from "socket.io-client";
+interface ServerToClientEvents {
+    noArg: () => void;
+    basicEmit: (a: number, b: string, c: Buffer) => void;
+    withAck: (d: string, callback: (e: number) => void) => void;
+  }
+  
+  interface ClientToServerEvents {
+    hello: () => void;
+  }
+  
+  // please note that the types are reversed
+  const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
+ //FIN DE USO DE //USO DE LIBRERIA PARA CLIENTE EN TYPESCRIPT DE IO SOCKET (ADEMAS INSTALAR npm install socket.io-client)
+
+ let agregarProducto:HTMLElement|null= document.getElementById("#productoAgregar");
+
+ if(agregarProducto){
+ agregarProducto.addEventListener("submit",(e)=>{
+    e.preventDefault();
+ });
+
+}
